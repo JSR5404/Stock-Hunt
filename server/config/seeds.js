72630802e84a8571363de553690c623152a1dfd8
chloegithub <miss_jihyun@hotmail.com>
2023-05-03 19:20:53 -0400

    amazon apple microsoft logo

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -20,7 +20,7 @@ db.once('open', async () => {
       name: 'Microsoft',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'stock-1.jpg',
+      image: 'microsoft.jpg',
       category: categories[0]._id,
       price: 2.99,
       quantity: 500
@@ -29,7 +29,7 @@ db.once('open', async () => {
       name: 'Apple',
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'stock-2.jpg',
+      image: 'apple.jpg',
       category: categories[0]._id,
       price: 1.99,
       quantity: 500
@@ -39,7 +39,7 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'stock-3.jpg',
+      image: 'amazon.jpg',
       price: 7.99,
       quantity: 20
     }
