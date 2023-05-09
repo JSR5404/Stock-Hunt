fcd1a4d0e5e760397c659fed872d9a6e8c61b70e
chloegithub <miss_jihyun@hotmail.com>
2023-05-09 17:00:34 -0400

    update Settings.js

--- client/src/components/pages/Settings.js
+++ client/src/components/pages/Settings.js
@@ -1,16 +1,18 @@
 import React from 'react';
-
+import Toggle from '../Toggle'
 const Settings = () => {
   return (
     <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Center',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
+    style={{
+      display: 'flex',
+      justifyContent: 'Center',
+      alignItems: 'Right',
+      height: '100vh'
+    }}
     >
       <h1>Settings</h1>
+      <br></br>
+      <Toggle/>
     </div>
   );
 };
