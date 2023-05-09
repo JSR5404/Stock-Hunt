f4fa6aaf7cb2e93ed5d0cc85c5697ed44ca5f9f2
skullkid4200 <jackeinhorn123@gmail.com>
2023-05-08 20:46:55 -0400

    added some of the major login/logout bones for the website.

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,44 @@ import Home from './components/pages/Home';
 import Dash from './components/pages/Dash';
 import Search from './components/pages/Search';
 import Settings from './components/pages/Settings';
+import Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <Navbar />
       <Routes>
@@ -18,9 +52,12 @@ function App() {
         <Route path='/dash' element={<Dash />} />
         <Route path='/search' element={<Search />} />
         <Route path='/settings' element={<Settings />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
 
     </Router>
+    </ApolloProvider>
   );
 }
 
