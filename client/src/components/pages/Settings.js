0ceac0a8b1ff8ce8a80b043143698793393f772d
chloegithub <miss_jihyun@hotmail.com>
2023-05-10 19:45:07 -0400

    dark mode theme

--- client/src/components/pages/Settings.js
+++ client/src/components/pages/Settings.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Toggle from '../Toggle'
 const Settings = () => {
   return (
     <div
@@ -14,6 +15,8 @@ const Settings = () => {
       <h1 className="font-serif">
         Settings
       </h1>
+      <br></br>
+      <Toggle/>
       </section>   
     </div>
   );
