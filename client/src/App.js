442b548f4c0471d31aabb352d653d9010bd60982
Pvon11 <patrickvonatbone@gmail.com>
2023-05-10 21:22:16 -0400

    Api working and search data

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import Navbar from './components/NavBar/index';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './components/pages/About';
-import Home from './components/pages/Home';
-import Dash from './components/pages/Dash';
-import Search from './components/pages/Search';
-import Settings from './components/pages/Settings';
-
+import React from "react";
+import Navbar from "./components/NavBar/index";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import Dash from "./components/pages/Dash";
+import Search from "./components/pages/Search";
+import Settings from "./components/pages/Settings";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/dash' element={<Dash />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/settings' element={<Settings />} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dash" element={<Dash />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
-
     </Router>
   );
 }
--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -1,47 +1,53 @@
-import React from 'react';
-
-
-
+import React, { useContext, useState } from "react";
+import SearchResults from "./SearchResults";
+import { searchSymbol } from "../../utils/api/stock-api";
 
 const Search = () => {
+  const [input, setInput] = useState("");
+
+  const [bestMatches, setBestMatches] = useState([]);
+
+  const updateBestMatches = async () => {
+    try {
+      if (input) {
+        const searchResults = await searchSymbol(input);
+        const result = searchResults.result;
+        setBestMatches(result);
+      }
+    } catch (error) {
+      setBestMatches([]);
+      console.log(error);
+    }
+  };
+
+  const clear = () => {
+    setInput("");
+    setBestMatches([]);
+  };
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Center',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-      <h1 className='text-5xl'>Search Stocks here.</h1>
-      <section>
-        {/* <section className='add-more-wrapper'>
-        <form>   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-        </div>
-        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required></input>
-        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+    <div className="flex bg-black">
+      <input
+        type="text"
+        value={input}
+        className="w-full px-4 py-2 focus:outline-none rounded-md"
+        placeholder="Search stock..."
+        onChange={(event) => setInput(event.target.value)}
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            updateBestMatches();
+          }
+        }}
+      />
+      {input && <button onClick={clear} className="m-1"></button>}
+      <button
+        onClick={updateBestMatches}
+        className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2 transition duration-300 hover:ring-2 ring-indigo-400"
+      ></button>
+      {input && bestMatches.length > 0 ? (
+        <SearchResults results={bestMatches} />
+      ) : null}
     </div>
-</form>
-                    <section>
-                    <div class="grid grid-rows-3 grid-flow-col gap-4">
-  <div class="row-span-3 ...">01</div>
-  <div class="col-span-2 ...">02</div>
-  <div class="row-span-2 col-span-2 ...">03</div>
-</div>
-                    </section>
-                    </section> */}
-      </section>
-
-
-
-    </div>
-
-
-
   );
 };
 
