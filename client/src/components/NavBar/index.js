c0679fb76344e9a2a3b4ea961fc06125877affed
skullkid4200 <jackeinhorn123@gmail.com>
2023-05-15 17:48:11 -0400

    some more style and functionality changes

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -65,52 +65,6 @@ const Navbar = () => {
     </NavMenu>
   </Nav>
   </div>
-  
-
-    // <>
-    //   <Nav>
-    //     {Auth.loggedIn()} ? (
-    //       <Bars />
-    //     <NavMenu>
-    //       <NavLink to='/' activestyle = "true">
-    //         Home
-    //       </NavLink>
-    //       <NavLink to='/dash' activestyle  = "true">
-    //         My Dash
-    //       </NavLink>
-    //       <NavLink to='/about' activestyle  = "true">
-    //         About
-    //       </NavLink>
-    //       <NavLink to='/search' activestyle  = "true">
-    //         Search Stocks
-    //       </NavLink>
-    //       <NavLink to='/settings' activestyle  = "true">
-    //         Settings
-    //       </NavLink>
-        
-    //     </NavMenu>
-    //   </Nav>
-    //     ) : (
-    //       <Bars />
-    //     <NavMenu>
-    //       <NavLink to='/' activestyle = "true">
-    //         Home
-    //       </NavLink>
-    //       <NavLink to='/about' activestyle  = "true">
-    //         About
-    //       </NavLink>
-    //       <NavLink to='/settings' activestyle  = "true">
-    //         Settings
-    //       </NavLink>
-    //       <NavLink to='/login' activestyle  = "true">
-    //         Log in
-    //       </NavLink>
-    //       <NavLink to='/signup' activestyle  = "true">
-    //         Sign Up
-    //       </NavLink>
-    //     </NavMenu>
-    //     )
-    // </>
   );
 };
   
